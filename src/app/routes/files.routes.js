3753ffd72d9ee325675cfc9c70c20057befdb0aa
rafaelhueb92 <rafaelhueb92@gmail.com>
2020-08-20 14:05:31 -0300

    remove cors

--- src/app/routes/files.routes.js
+++ src/app/routes/files.routes.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 const multer = require("multer");
-const cors = require("cors")
+
 const fileController = require("../controllers/files.controller");
 const filesController = require("../controllers/files.controller");
 
@@ -8,6 +8,6 @@ const uploadConfigs = require("../../core/utils/multer.utils");
 const upload = multer(uploadConfigs);
 
 routes.get("/", fileController.list);
-routes.post("/",cors(), upload.single("file"), filesController.upload);
+routes.post("/",upload.single("file"), filesController.upload);
 
 module.exports = routes;
